19ea6c8cde48f1008760e4d31e9f2abcf432eccc
choijw1004 <97choijw@gmail.com>
2026-02-03 13:47:55 +0900

    feat: LeetResponseDto

---
+++ core-api/src/main/java/com/psorganizer/psorganizer/domain/leetcode/LeetResponseDto.java
@@ -0,0 +1,9 @@
+package com.psorganizer.psorganizer.domain.leetcode;
+
+import lombok.Data;
+
+@Data
+public class LeetResponseDto {
+    private String questionId;
+    private String questionTitle;
+}
