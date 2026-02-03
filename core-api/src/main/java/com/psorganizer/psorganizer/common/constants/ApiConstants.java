d09816b92ca2a85925011fbab0fd74d35eb63ccf
choijw1004 <97choijw@gmail.com>
2026-02-03 12:35:12 +0900

    feat: BOJ_BASE_URL 수정

--- core-api/src/main/java/com/psorganizer/psorganizer/common/constants/ApiConstants.java
+++ core-api/src/main/java/com/psorganizer/psorganizer/common/constants/ApiConstants.java
@@ -3,6 +3,6 @@ package com.psorganizer.psorganizer.common.constants;
 public class ApiConstants {
     private ApiConstants(){}
 
-    public static final String BOJ_BASE_URL = "https://solved.ac/api/v3?problemId=";
+    public static final String BOJ_BASE_URL = "https://solved.ac/api/v3/problem/show?problemId=";
 
 }
