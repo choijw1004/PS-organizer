41569a4afb21c59b60f1a41e3c3b5dff611d2312
choijw1004 <97choijw@gmail.com>
2026-02-03 11:59:55 +0900

    init: Project Init

---
+++ core-api/src/test/java/com/psorganizer/psorganizer/CoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psorganizer.psorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
