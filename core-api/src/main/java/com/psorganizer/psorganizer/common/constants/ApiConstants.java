b33f1503e54124fc1df7e73f840f5db76388259f
choijw1004 <97choijw@gmail.com>
2026-02-03 13:48:32 +0900

    feat: LEET_BASE_URL 외부 api에서 로컬 api 방식으로 변경

--- core-api/src/main/java/com/psorganizer/psorganizer/common/constants/ApiConstants.java
+++ core-api/src/main/java/com/psorganizer/psorganizer/common/constants/ApiConstants.java
@@ -5,4 +5,5 @@ public class ApiConstants {
 
     public static final String BOJ_BASE_URL = "https://solved.ac/api/v3/problem/show?problemId=";
 
+    public static final String LEET_BASE_URL = "http://localhost:3001/select?titleSlug=";
 }
