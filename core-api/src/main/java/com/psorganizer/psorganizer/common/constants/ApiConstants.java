e9ff12d4c7e8804a32050d55384a10392996f5f6
choijw1004 <97choijw@gmail.com>
2026-02-16 18:16:15 +0900

    fix: LeetCode baseURL 변경

--- core-api/src/main/java/com/psorganizer/psorganizer/common/constants/ApiConstants.java
+++ core-api/src/main/java/com/psorganizer/psorganizer/common/constants/ApiConstants.java
@@ -5,5 +5,5 @@ public class ApiConstants {
 
     public static final String BOJ_BASE_URL = "https://solved.ac/api/v3/problem/show?problemId=";
 
-    public static final String LEET_BASE_URL = "http://localhost:3001/select?titleSlug=";
+    public static final String LEET_BASE_URL = "http://host.docker.internal:3001/select?titleSlug=";
 }
